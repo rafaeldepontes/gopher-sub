c4d404b3f1f4c59c5b45116572a81580c85833df
Rafael Carneiro <rafael.cr.carneiro@gmail.com>
2025-12-29 15:51:14 -0300

    feat: using controllers on the handler

--- backend/internal/middleware/handler.go
+++ backend/internal/middleware/handler.go
@@ -3,17 +3,25 @@ package middleware
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/rafaeldepontes/gopher-sub/internal/auth"
+	authControl "github.com/rafaeldepontes/gopher-sub/internal/auth/controller"
 	"github.com/rafaeldepontes/gopher-sub/internal/logger"
+	"github.com/rafaeldepontes/gopher-sub/internal/subscription"
+	subControl "github.com/rafaeldepontes/gopher-sub/internal/subscription/controller"
 	"github.com/sirupsen/logrus"
 )
 
 type handle struct {
-	Log *logrus.Logger
+	authController auth.Controller
+	subController  subscription.Controller
+	Log            *logrus.Logger
 }
 
 func NewHandle() *handle {
 	return &handle{
-		Log: logger.InitLogger(),
+		authController: authControl.NewController(),
+		subController:  subControl.NewController(),
+		Log:            logger.GetLogger(),
 	}
 }
 
@@ -21,6 +29,8 @@ func (h *handle) ConfigHandler(r *chi.Mux) {
 	r.Use(middleware.StripSlashes)
 
 	r.Route("/api/v1", func(r chi.Router) {
-
+		r.Post("/login", h.authController.Login)
+		r.Post("/register", h.authController.Register)
+		r.Post("/subscribe/{id}", h.subController.Subscriber)
 	})
 }
