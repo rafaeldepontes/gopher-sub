5a593617569d7abc17431d5e1386999173194ce6
Rafael Carneiro <rafael.cr.carneiro@gmail.com>
2025-12-30 13:02:16 -0300

    feat: added a real handler for each request

--- backend/internal/middleware/handler.go
+++ backend/internal/middleware/handler.go
@@ -1,39 +1,53 @@
 package middleware
 
 import (
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"github.com/rafaeldepontes/gopher-sub/internal/auth"
-	authControl "github.com/rafaeldepontes/gopher-sub/internal/auth/controller"
+	"encoding/base64"
+	"net/http"
+	"strings"
+
 	"github.com/rafaeldepontes/gopher-sub/internal/logger"
-	"github.com/rafaeldepontes/gopher-sub/internal/subscription"
-	subControl "github.com/rafaeldepontes/gopher-sub/internal/subscription/controller"
-	"github.com/sirupsen/logrus"
 )
 
-type handle struct {
-	authController auth.Controller
-	subController  subscription.Controller
-	Log            *logrus.Logger
-}
+func AuthenticationFilter(next http.Handler) http.Handler {
 
-func NewHandle() *handle {
-	return &handle{
-		authController: authControl.NewController(),
-		subController:  subControl.NewController(),
-		Log:            logger.GetLogger(),
-	}
-}
+	// I'm using base64 to enconde and decode the token because the focus here is not
+	// security, I'm just trying to make a system with an email funcionality that it
+	// will never be in production...
+	//
+	// If you want to see something that really uses the newest tecnologies for
+	// token protection, check: github.com/rafaeldepontes/fauthless-go
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var accessType []byte
 
-func (h *handle) ConfigHandler(r *chi.Mux) {
-	r.Use(middleware.StripSlashes)
+		dirtyToken := r.Header.Get("Authorization")
+		if dirtyToken == "" {
+			logger.GetLogger().Errorln("Missing token")
+			http.Error(w, "Missing token", http.StatusUnauthorized)
+			return
+		}
 
-	// TODO: Do a middleware for actual authentication
-	// TODO: Create the email service
+		token := cleanUpToken(dirtyToken)
 
-	r.Route("/api/v1", func(r chi.Router) {
-		r.Post("/login", h.authController.Login)
-		r.Post("/register", h.authController.Register)
-		r.Post("/subscribe/{id}", h.subController.Subscriber)
+		accessType, err := base64.RawURLEncoding.DecodeString(token)
+		if err != nil {
+			logger.GetLogger().Errorln(err)
+			http.Error(w, "Something went wrong, try again later...", http.StatusInternalServerError)
+			return
+		}
+
+		if string(accessType) != "allowed" {
+			logger.GetLogger().Errorln("User not allowed")
+			http.Error(w, "User not allowed...", http.StatusUnauthorized)
+			return
+		}
+		next.ServeHTTP(w, r)
 	})
+}
+
+func cleanUpToken(tk string) string {
+	result, found := strings.CutPrefix(tk, "Bearer ")
+	if !found {
+		return tk
+	}
+	return result
 }
