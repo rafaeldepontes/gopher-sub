6c0021fa48cc2591908b113f5fb6adaf74191521
Rafael Carneiro <rafael.cr.carneiro@gmail.com>
2026-01-01 16:09:48 -0300

    fix: setting up the cors configuration

--- backend/internal/middleware/routes.go
+++ backend/internal/middleware/routes.go
@@ -1,8 +1,11 @@
 package middleware
 
 import (
+	"os"
+
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
 	"github.com/rafaeldepontes/gopher-sub/internal/auth"
 	authControl "github.com/rafaeldepontes/gopher-sub/internal/auth/controller"
 	"github.com/rafaeldepontes/gopher-sub/internal/logger"
@@ -26,15 +29,23 @@ func NewHandle() *handle {
 }
 
 func (h *handle) ConfigHandler(r *chi.Mux) {
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{os.Getenv("FRONTEND_URL")},
+		AllowedMethods:   []string{"POST", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type"},
+		ExposedHeaders:   []string{"Authorization"},
+		AllowCredentials: false,
+		MaxAge:           300, //5 min
+	}))
 	r.Use(middleware.StripSlashes)
 
-	r.Post("/api/v1/login", h.authController.Login)
-	r.Post("/api/v1/register", h.authController.Register)
-
 	r.Route("/api/v1", func(r chi.Router) {
-		r.Use(AuthenticationFilter)
-		// TODO: Create the email service
-		// WIP
-		r.Post("/subscribe/{id}", h.subController.Subscriber)
+		r.Post("/login", h.authController.Login)
+		r.Post("/register", h.authController.Register)
+
+		r.Group(func(r chi.Router) {
+			r.Use(AuthenticationFilter)
+			r.Post("/subscribe/{id}", h.subController.Subscriber)
+		})
 	})
 }
